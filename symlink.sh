846b7452dca690655713571a09605c5a5736a1b9
Stefan <stefan.schneider@intergga.ch>
2015-01-29 23:48:08 +0100

    replaced grml-zsh-config with oh-my-zsh

--- symlink.sh
+++ symlink.sh
@@ -2,25 +2,34 @@
 ## Little script that symlinks config files to home directory.
 ## Warning! This overwrites existing configurations!
 
+PWD=`pwd`
+
 # link all dotfiles to home root
 sync_dotfiles() {
   echo "Symlinking dotfiles..."
   for d in ./.*
   do
     if [[ -f $f ]]; then
-    ln -sf $f ~/
+    ln -sf $f $HOME/
   fi
   done
 }
 
-# link files in config dir separately
+# link files from config dir separately
 sync_config() {
   echo "Symlinking config dir..."
   mkdir -p ~/.config/beets/
-  ln -sf ./config/beets/config.yml ~/.config/beets/
+  ln -sf $PWD/beets/config.yml $HOME/.config/beets/
+
+  mkdir -p ~/.config/zsh/
+  ln -sf $PWD/config/zsh/antigen.zsh $HOME/.config/zsh/
+
+  ln -sf $PWD/.oh-my-zsh $HOME/
 }
 
 cleanup() {
+  # clean up dead symlinks
+  for i in $(file ./* | grep broken | cut -d : -f 1); do rm $i; done
   echo "Finished!"
 }
 
