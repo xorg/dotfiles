0598d1054cee697ed30f1f8d3904068ca9ad1b23
Stefan <stefan.schneider@intergga.ch>
2015-01-29 10:05:07 +0100

    Refactored symlink script

--- symlink.sh
+++ symlink.sh
@@ -2,9 +2,29 @@
 ## Little script that symlinks config files to home directory.
 ## Warning! This overwrites existing configurations!
 
-# sync all dotfiles to home root
-ln -sf ./.* ~/
+# link all dotfiles to home root
+sync_dotfiles() {
+  echo "Symlinking dotfiles..."
+  for d in ./.*
+  do
+    if [[ -f $f ]]; then
+    ln -sf $f ~/
+  fi
+  done
+}
 
-# sync files in config dir separately
-mkdir ~/.config/beets/
-ln -sf ./.config/beets/config.yml ~/.config/beets/
+# link files in config dir separately
+sync_config() {
+  echo "Symlinking config dir..."
+  mkdir -p ~/.config/beets/
+  ln -sf ./config/beets/config.yml ~/.config/beets/
+}
+
+cleanup() {
+  echo "Finished!"
+}
+
+
+sync_dotfiles
+sync_config
+cleanup
