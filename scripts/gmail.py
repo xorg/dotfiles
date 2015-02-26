a20a92822229e6e38f5f7a68b5ae8f4ff550d70c
Stefan Schneider <stefan.schneider@intergga.ch>
2015-02-26 16:06:10 +0100

    Trying to get vim-airline to work

--- scripts/gmail.py
+++ scripts/gmail.py
@@ -3,8 +3,8 @@
 from urllib.request import FancyURLopener
 import os
 
-email='xorg112' 
-#password=password
+email='xorg112@' 
+password='qasw12-google'
 
 url = 'https://%s:%s@mail.google.com/mail/feed/atom' % (email, password)
 
@@ -18,7 +18,8 @@ ito   = contents.index('</fullcount>')
 
 fullcount = contents[ifrom:ito]
 
-if int(fullcount) > 0:
-    print(fullcount + ' new mails')
+if int(fullcount) == 1:
+    print('one new mail')
+elif:
 else:
     print('no new mails')
